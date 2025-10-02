4cf7731e67038bc970a80854848349d8b867f265
Matias Saavedra <matias.saavedra.c@gmail.com>
2025-10-02 17:46:57 -0300

    Completes model.py.

--- events/models.py
+++ events/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    user_id = models.BigAutoField(primary_key=True) 
     username = models.CharField(max_length=30)
 
 
@@ -16,8 +17,16 @@ class Events(models.Model):
     description = models.TextField(blank=True)
     created = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
     date = models.DateField(blank=True)
-    # category = models.ForeignKey(Category, default="general", on_delete=models.CASCADE)  # la llave foránea
+    category = models.ForeignKey(Category, default="general", on_delete=models.CASCADE)  # la llave foránea
     owner = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+class Category(models.Model):
+    cat_id = models.BigAutoField(primary_key=True)
+    category = models.CharField(max_length=50)
+
+class HasSubs(models.Model):
+    username = models.ForeignKey(User)
+    name = models.ForeignKey(Events)
