0346082e11cf25fb153cb5869f978a4d089b05a3
King-PR <pabloreyespomes@gmail.com>
2025-10-11 20:28:19 -0300

    feat: agregar URL para crear eventos

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -5,5 +5,6 @@ app_name = 'events'
 
 urlpatterns = [
     path('', views.event_list, name='event_list'),
+    path('crear/', views.event_create, name='event_create'),
     path('test/', views.test_view, name='test'),
 ]
