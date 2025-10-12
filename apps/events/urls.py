6b9cdddb5753d483ed3c58234d713260451875f2
King-PR <pabloreyespomes@gmail.com>
2025-10-11 22:21:54 -0300

    feat: URL de detalle de evento actualizado

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -6,5 +6,6 @@ app_name = 'events'
 urlpatterns = [
     path('', views.event_list, name='event_list'),
     path('crear/', views.event_create, name='event_create'),
+    path('evento/<int:event_id>/', views.event_detail, name='event_detail'),
     path('test/', views.test_view, name='test'),
 ]
