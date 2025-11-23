61a2a625a5603ab5a18a523c42f24c46022adb1a
DiegoArredo <diego.arredo18@gmail.com>
2025-11-22 23:21:40 -0300

    feat: Se añaden endpoints para suscribe y unsuscribe.

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.event_list, name='event_list'),
     path('crear/', views.event_create, name='event_create'),
     path('evento/<int:event_id>/', views.event_detail, name='event_detail'),
+    path('subscribe/', views.subscribe, name='event_subscribe'),
+    path('unsubscribe/', views.unsubscribe, name='event_unsubscribe'),
     path('test/', views.test_view, name='test'),
 ]
